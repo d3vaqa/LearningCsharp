9601a69b286752c65de41b073665050ef8de3392
Hiro <d3vaqa@gmail.com>
2023-10-05 14:26:20 +1100

    first day of c sharp

---
+++ BasicMath/BasicMath/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicMath
+{
+    internal class Dog
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public void Bark()
+        {
+            Console.WriteLine(Name + "says: Woof! Woof!");
+        }
+
+        public void Fetch(string thing)
+        {
+            Console.WriteLine($"{Name} fetched the {thing}");
+        }
+
+    }
+}
---
+++ BasicMath/BasicMath/Program.cs
@@ -0,0 +1,25 @@
+﻿// Tells the computer to use a collection of basic functions provided by C#
+using System;
+
+
+// namespace is like a container for our program 
+namespace BasicMath
+
+{
+    // Think of a class as a blueprint. Here, Program is a blueprint for a tiny program we’re making.
+    class Program
+    {
+        //This line defines a function named Main. 
+        static void Main(string[] args)
+        {
+
+            Dog myDog = new Dog();
+            myDog.Name = "Rex";
+            myDog.Color = "Brown";
+
+            myDog.Bark();
+            myDog.Fetch("Ball");
+            
+        }
+    }
+}
